06097305a626c8563b750af86350548e64db56b9
Roberto Quintana <robertofauter@gmail.com>
2024-11-20 20:47:13 +0100

    Primeros pasos en JS

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,11 @@
+// "let" es una palabra reservada para definir variables
+let nombre ="Hola Mundo"; 
+let NombreCompleto; //convencio UpperCamelCase
+let nombreCompleto; //convecion camelCase  "esta es la q se usa normalmente"
+let nombre_completo; //convencion snake_case
+console.log(nombre);
+
+let apellido;
+let animal;
+apellido = 'Quintana';
+
