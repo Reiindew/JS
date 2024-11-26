cdf513d912eff533654eaf3f329630b80d2caab5
Roberto Quintana <robertofauter@gmail.com>
2024-11-26 11:00:30 +0100

    Siguiendo con la pelea

---
+++ 02-operadores/03-comparacion.js
@@ -0,0 +1,11 @@
+let a = 10;
+
+console.log(a > 5); //estos son comparadores relacionales
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+console.log(a == 10); //estos son comparadores de igualdad
+console.log(a != 10);
+console.log(a === '10');
+console.log(a !== '10');
---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,16 @@
+// and, or, not
+
+//and &&
+
+let mayor = false;
+let suscrito = true;
+// console.log(true && true);
+// console.log(false && true);
+console.log('operador and', mayor && suscrito); //los dos tienen que ser true
+
+//or ||
+console.log('operador or', mayor || suscrito); //uno de ellos tiene que ser true
+
+// not !
+console.log('operador not', !mayor); // nos muestra la respuesta contraria 
+let soloCatalogoInfantil = !mayor;
